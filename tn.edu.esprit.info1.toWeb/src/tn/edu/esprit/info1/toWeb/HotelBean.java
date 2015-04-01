6e685dbd7a4f4bcc8dfe1a866bd3b18ef39320d4
kbenhddia <benhddia-khalil@live.fr>
2015-04-01 17:09:27 +0100

    produit hotel
    Signed-off-by: kbenhddia <benhddia-khalil@live.fr>

---
+++ tn.edu.esprit.info1.toWeb/src/tn/edu/esprit/info1/toWeb/HotelBean.java
@@ -0,0 +1,44 @@
+package tn.edu.esprit.info1.toWeb;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+import domain.Hotel;
+import services.interfaces.HotelServiceLocal;
+
+@ManagedBean 
+@RequestScoped
+public class HotelBean {
+
+	@EJB
+	HotelServiceLocal hotelServiceLocal;
+	
+	private List<Hotel> hotels;
+	
+	private Hotel selectedHotel = new Hotel();
+	
+	public List<Hotel> getHotels() {
+		return hotelServiceLocal.getAllHotel();
+	}
+
+	public void setHotels(List<Hotel> hotels) {
+		this.hotels = hotels;
+	}
+
+	public Hotel getSelectedHotel() {
+		return selectedHotel;
+	}
+
+	public void setSelectedHotel(Hotel selectedHotel) {
+		this.selectedHotel = selectedHotel;
+	}
+	
+	public void doAddHotel(){
+		hotelServiceLocal.addHotel(selectedHotel);
+	}
+	
+}
